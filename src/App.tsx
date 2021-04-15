7bd4eccf4094a060d4c1a3aa3adfde1aee5cc545
shinji0320 <shinji.blue.one@gmail.com>
2021-04-15 13:07:23 +0900

    [add]Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-
 import React from "react";
 import styles from "./App.module.scss";
 import Header from "./componets/header/Header";
+import TackForm from "./features/task/taskForm/TaskForm";
 
 const App: React.FC = () => {
   return (
     <div className={styles.root}>
       <div className={styles.wrapper}>
         <Header />
+        <TackForm />
       </div>
     </div>
   );
--- src/features/task/taskForm/TaskForm.tsx
+++ src/features/task/taskForm/TaskForm.tsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import styles from "./TaskForm.module.scss";
+import TextField from "@material-ui/core/TextField";
+
+type Inputs = {
+  taskTitle: string;
+};
 
 const TaskForm: React.FC = () => {
-  return <div></div>;
+  const { register, handleSubmit, reset } = useForm();
+  const handleCreate = (data: Inputs) => {
+    console.log(data);
+    reset();
+  };
+  return (
+    <div className={styles.root}>
+      <form onSubmit={handleSubmit(handleCreate)} className={styles.form}>
+        <TextField
+          id="outlined-basic"
+          label="New Task"
+          variant="outlined"
+          inputRef={register}
+          name="taskTitle"
+          className={styles.text_field}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default TaskForm;
